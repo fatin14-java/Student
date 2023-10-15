01fa329427c3c622a78ff52b34e8712d7f3d7a35
User <User@LAPTOP-QUFJE5SL>
2023-10-16 00:42:32 +0800

    First Commit

---
+++ Student/src/com/studyopedia/Student.java
@@ -0,0 +1,11 @@
+package com.studyopedia;
+
+public class Student {
+    public static void main(String[] args) {
+        
+        System.out.println("Nurul Fatin Amira Binti Norizan" );
+        System.out.println("216682");
+        System.out.println("B17 04-04 Jalan Desa Mutiara 11 Taman Desa Mutiara Jb Johor");
+    }
+}
+
